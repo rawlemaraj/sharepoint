cb2152666024b51aef266ecb70afab76930d4870
rawlemaraj <rawlemaraj@hotmail.com>
2023-05-19 17:10:38 -0400

    third

--- accessing_sharepoint6.py
+++ accessing_sharepoint6.py
@@ -1,34 +1,27 @@
 from shareplum import Site
 from shareplum import Office365
 from shareplum.site import Version
-from requests_ntlm import HttpNtlmAuth
-import requests
-
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # setup parameters
 username = 'username'  # username for SharePoint
 password = 'password'  # password for SharePoint
 site_url = 'https://sharepoint-site-url'  # SharePoint Site URL
-file_url = 'path-to-file-in-sharepoint'  # URL to the file you want to download
+folder_path = 'path-to-folder-in-sharepoint'  # Path to the folder containing your file
+file_name = 'file-name'  # The name of the file you want to download
 local_file_path = 'path-to-local-file'  # Local path where file needs to be saved
 
-# Setup session with NTLM Authentication
-session = requests.Session()
-session.auth = HttpNtlmAuth(username, password)
-
-# Setup Shareplum site
+# authenticate and setup connection to SharePoint
 authcookie = Office365(site_url, username=username, password=password).GetCookies()
 site = Site(site_url, version=Version.v2019, authcookie=authcookie)
 
-# Open the SharePoint file
-response = session.get(file_url, stream=True, verify=False)
+# get SharePoint folder
+sp_folder = site.Folder(folder_path)
 
-# Save the file locally
+# download the file
+file_content = sp_folder.get_file(file_name)
+
+# save the file locally
 with open(local_file_path, 'wb') as out_file:
-    for chunk in response.iter_content(chunk_size=1024):
-        if chunk:
-            out_file.write(chunk)
+    out_file.write(file_content)
 
 print(f"File downloaded to {local_file_path}")
